639aa6c1d8d7baf4b494b8879238205dcaf43af2
Gautam Bhuyan <gautam.bhuyan@gmail.com>
2025-06-28 13:45:08 -0400

    Update dictSearch.py

--- dictSearch.py
+++ dictSearch.py
@@ -1,45 +1,46 @@
 
-def createGraph(dictionary: [str]) :
+# Find the path from one to another in a dictionary, such that each adjascent word differs from the next by one character
+# The Code first creates a graph of the dictionary where the nearest neighbors differ by maximum one character. Then if employs depth first search to find the laddar or words.
 
-    for i in range(len(dictionary)):
-        for j in range(len(dictionary)):
-            if i == j:
-                continue
-            w1 = dictionary[i]
-            w2 = dictionary[j]
 
-            diff = 0
-
-            #print("Comparing:", i, j, w1, w2, graph[i][j])
-
-            for k in range(len(w1)):
-                if w1[k] != w2[k]:
-                    diff += 1
-            if diff <= 1:
-                graph[i][j] = 1
-                graph[j][i] = 1
-
-    return 
-
-def qualify(a: str, b: str) -> bool:
+def qualify(a: str, b: str) -> int:
 
     diffCount = 0 
 
     for i in range(len(a)):
         if a[i] != b[i]:
             diffCount += 1
-    if diffCount <= 1:
-        return True
-    return False
+    
+    return diffCount
+    
+def createGraph(dictionary: [str]) :
+
+    for i in range(len(dictionary)):
+        for j in range(i + 1, len(dictionary)):
+            if i == j:
+                continue
+            w1 = dictionary[i]
+            w2 = dictionary[j]
+
+            if qualify(w1, w2) <= 1:
+                graph[i][j] = 1
+                graph[j][i] = 1
+
+    return 
+
 
 def shortestPath(p1: str, p2: str, dict: [str], graph: [[int]]) -> [int]:
 
-    start = dict.index(p2)
+    try :
+        start = dict.index(p2)
+    except ValueError:
+        print("Error:", p2, "not in dictionary")
+        return []
+
     stack = []
     stkPtr = -1
     dictLen = len(dict)
     parent = [0 for _ in range(dictLen)]
-
     visited = [0 for _ in range(dictLen)]
 
     j = start
@@ -60,17 +61,19 @@ def shortestPath(p1: str, p2: str, dict: [str], graph: [[int]]) -> [int]:
                 parent[i] = j
                 #print("### Parent of", dict[i], "is", dict[j])
                 
-                if qualify(dict[i], p1):
-                    #print("Found a match:", dict[i], "Parent:", p1)
+                q = qualify(dict[i], p1)
+                if q <= 1:
+                    print("The word laddar is as follows:")
                     k = i
-                    print(p1)
+                    if q == 1:
+                        print(p1)
                     
                     while parent[k] != 0:
                         print(dict[k])
                         k = parent[k]
                     print(p2)
                     done = True
-                    return parent        
+                    return parent   # if the printing business needs to happen in the caller function     
         depth += 1
         if depth > dictLen:
             return []
@@ -88,10 +91,12 @@ graph = [[0 for _ in range(len(dict))] for _ in range(len(dict))]
 
 createGraph(dict)
 
-P1 = "pot"
-P2 = "sop"
+P1 = "lop"
+P2 = "cog"
 
 print(P1, " to ", P2, " in dictionary: ", dict)
 
 path = shortestPath(P1, P2, dict, graph)
+if not path:
+    print("No path found from", P1, "to", P2)
 #print("Shortest path from", P1, "to", P2, ":", path)
